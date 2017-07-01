e5af7b0112a17254970169a563e21dbc02f7c261
niq77avie <niq77avie@yahoo.com>
2017-07-01 17:29:26 +0800

    Initial commit

---
+++ src/main/java/com/comfarm/gardnr/GardnrApplication.java
@@ -0,0 +1,12 @@
+package com.comfarm.gardnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardnrApplication.class, args);
+	}
+}
